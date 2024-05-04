577e85260421947bf6b79a4bb23c40d03cadf4ae
Mana <manaabdo333@gmail.com>
2024-05-05 02:33:53 +0300

    Added some todos

--- init.lua
+++ init.lua
@@ -43,6 +43,8 @@ What is Kickstart?
 
 Kickstart Guide:
 
+  TODO: Add Auto pair
+  TODO: Add something for git
   TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
 
     If you don't know what this means, type the following:
@@ -567,6 +569,7 @@ require('lazy').setup({
         tsserver = {},
         --
 
+        -- Love me some emmet
         emmet_language_server = {},
 
         lua_ls = {
@@ -788,20 +791,29 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 
-      -- You can configure highlights by doing something like:
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'carbonfox'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
