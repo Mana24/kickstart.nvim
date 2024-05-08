af49d16be59239a9e555876fd32f50d7d9365cb5
Mana <manaabdo333@gmail.com>
2024-05-08 19:42:57 +0300

    added a color to operators in duskfox

--- init.lua
+++ init.lua
@@ -761,6 +761,15 @@ require('lazy').setup({
   {
     'EdenEast/nightfox.nvim',
     priority = 1000,
+    opts = {
+      specs = {
+        all = {
+          syntax = {
+            operator = 'yellow.dim',
+          },
+        },
+      },
+    },
     init = function()
       vim.cmd.colorscheme 'duskfox'
       vim.cmd.hi 'Comment gui=none'
